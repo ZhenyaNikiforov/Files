b7161706ac67321f43061a1fdaf697396b94b2f7
Zhenya Nikiforov <12A92@mail.ru>
2025-08-14 21:35:19 +0500

    Окончательный вариант первого задания.

--- Zadacha_1/main.cpp
+++ Zadacha_1/main.cpp
@@ -1,19 +1,24 @@
-/* Zadacha 1. */
+/* Задача 1.
+Читаем надписи из файла. */
 
 #include <iostream>
 #include <fstream>
+#include <windows.h>
 #include "paths.cpp"
 #include "hard.cpp"
 using namespace std;
 
 int main()
 {
+  SetConsoleOutputCP(CP_UTF8);
+
   string vegetable = "";
   ifstream vegetables(pathToVegetables);
 
   if (!vegetables.is_open())
   {
     cout << fail << "\n";
+    vegetables.close();
     return 0;
   }
 
@@ -23,5 +28,6 @@ int main()
     cout << vegetable << "\n";
   }
 
+  vegetables.close();
   return 0;
 }
